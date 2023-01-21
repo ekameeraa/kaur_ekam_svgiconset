5ef115f96b3dcd32131f1bd09ddd4ed7672a334d
ekampreetkaur <e_ekampreetkaur@fanshaweonline.ca>
2023-01-20 20:02:14 -0500

    update js folder
    doing main.js

--- js/main.js
+++ js/main.js
@@ -1,31 +1,31 @@
 console.log('Javascript is running');
 
-let vector = document.querySelector("#vectorAnchor");
-let vectorBag = document.querySelector("#vectorBag");
-let vectorCoctail = document.querySelector("#vectorCoctail");
-let vectorHotel = document.querySelector("#vectorHotel");
-let vectorIsland = document.querySelector("#vectorIsland");
-let vectorLifesaver = document.querySelector("#vectorLifesaver");
-let vectorMap = document.querySelector("#vectorMap");
+let vectorBeach = document.querySelector("#vectorBeach");
+let vectorBridge = document.querySelector("#vectorBridge");
+let vectorCity = document.querySelector("#vectorCity");
+let vectorDesert = document.querySelector("#vectorDesert");
+let vectorFall = document.querySelector("#vectorFall");
+let vectorForest = document.querySelector("#vectorForest");
+let vectorHimalaya = document.querySelector("#vectorHimalaya");
+let vectorHome = document.querySelector("#vectorHome");
 let vectorMountain = document.querySelector("#vectorMountain");
-let vectorPlane = document.querySelector("#vectorPlane");
-let vectorSandals = document.querySelector("#vectorSandals");
-let vectorShip = document.querySelector("#vectorShip");
-let vectorTent = document.querySelector("#vectorTent");
+let vectorRiver = document.querySelector("#vectorRiver");
+let vectorScenery = document.querySelector("#vectorScenery");
+let vectorSea = document.querySelector("#vectorSea");
 
 function logID() {
     console.log(this.id);
 }
 
-vectorAnchor.addEventListener('click', logID);
-vectorBag.addEventListener('click', logID);
-vectorCoctail.addEventListener('click', logID);
-vectorHotel.addEventListener('click', logID);
-vectorIsland.addEventListener('click', logID);
-vectorLifesaver.addEventListener('click', logID);
-vectorMap.addEventListener('click', logID);
+vectorBeach.addEventListener('click', logID);
+vectorBridge.addEventListener('click', logID);
+vectorCity.addEventListener('click', logID);
+vectorDesert.addEventListener('click', logID);
+vectorFall.addEventListener('click', logID);
+vectorForest.addEventListener('click', logID);
+vectorHimalaya.addEventListener('click', logID);
+vectorHome.addEventListener('click', logID);
 vectorMountain.addEventListener('click', logID);
-vectorPlane.addEventListener('click', logID);
-vectorSandals.addEventListener('click', logID);
-vectorShip.addEventListener('click', logID);
-vectorTent.addEventListener('click', logID);
+vectorRiver.addEventListener('click', logID);
+vectorScenery.addEventListener('click', logID);
+vectorSea.addEventListener('click', logID);
